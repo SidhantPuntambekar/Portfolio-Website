e2d7077975fb29daed82cc47ff7fa0140fb219b7
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-05-14 12:56:32 -0600

    Added computational biology page

---
+++ src/pages/ComputationalBiology.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default () => (
+    <Layout>
+        <h2>Computational Biology</h2>
+        <p>This page serves as a collection of fascinating things I learn in Computational Biology.</p>
+    </Layout>
+)
