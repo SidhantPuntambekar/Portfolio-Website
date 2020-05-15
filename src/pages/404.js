3329d8f15d66de40a86608056b6b7b7b027562e7
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-05-15 09:57:31 -0600

    Added travis

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default () => (
+    <Layout>
+        <p>Welcome to the 404 page. Please try a different url.</p>
+    </Layout>
+)
