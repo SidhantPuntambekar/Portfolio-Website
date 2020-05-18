c4e4c96054caa8a733166d58e9a0f3e88cb998f8
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-05-18 10:12:50 -0600

    Redefined resume path

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,6 +3,6 @@ import Layout from "../components/Layout";
 
 export default () => (
     <Layout>
-        <p>Check out my resume here: <iframe src="./ResumePuntambekarWebsite.pdf" width="100%" height="500px"></iframe> or my GitHub at: <a href="https://www.github.com/SidhantPuntambekar">GitHub</a></p>
+        <p>Check out my resume here: <iframe src="../static/ResumePuntambekarWebsite.pdf" width="100%" height="500px"></iframe> or my GitHub at: <a href="https://www.github.com/SidhantPuntambekar">GitHub</a></p>
     </Layout>
 )
