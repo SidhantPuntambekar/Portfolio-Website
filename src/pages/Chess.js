2e14802259839ccfe90e1a7886fb5ef11957556b
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-12 15:28:02 -0600

    Added new images

--- src/pages/Chess.js
+++ src/pages/Chess.js
@@ -24,6 +24,14 @@ export default () =>
                 <img height = "200" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Bobby_Fischer_1972.jpg/200px-Bobby_Fischer_1972.jpg"></img>
                 <figcaption>Robert James Fischer, World Chess Championship Challenger, USA</figcaption>
             </figure>
+            <figure>
+                <img height = "200" src = "https://lh3.googleusercontent.com/proxy/jAOyJ-5zp3DqkoNZ_EstL2ua7YzZC8Bw8-e0GU8iG66Pp3hQ3dxJcgEwQ4JF-mfs7EcWw-jt5uN2fhy8uFOfkHOAirZtc9-XIJ_ziHvd6za5YdZPru0Wp-hQdc7uLA"></img>
+                <figcaption>Jim Slater</figcaption>
+            </figure>
+            <figure>
+                <img height = "200" src = "https://www.chesshistory.com/winter/pics/cn5198_fischer.jpg"></img>
+                <figcaption>Newspaper headline indicating the increase of prize fund money.</figcaption>
+            </figure>
         </p>
     </Layout>
 )
