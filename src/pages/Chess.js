df071cbd35e17b447b2dc802b99ba01e4fb68438
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-12 15:22:06 -0600

    Added background section

--- src/pages/Chess.js
+++ src/pages/Chess.js
@@ -11,7 +11,19 @@ export default () =>
         </p>
         <h2>1972 World Chess Championship</h2>
         <p>
-            
+            <h3>Background</h3>
+            <i><b>If money is the question, here it is. Now come out and play, chicken!
+            <br></br>-Jim Slater</b></i>
+            <br></br>
+            <br></br>
+            <figure>
+                <img height = "200" src = "https://d53j7mnhlzrdg.cloudfront.net/series/best-games-of-boris-spassky.jpg"></img>
+                <figcaption>Boris Vasiljevic Spassky, World Chess Defending Champion, USSR</figcaption>
+            </figure>
+            <figure>
+                <img height = "200" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Bobby_Fischer_1972.jpg/200px-Bobby_Fischer_1972.jpg"></img>
+                <figcaption>Robert James Fischer, World Chess Championship Challenger, USA</figcaption>
+            </figure>
         </p>
     </Layout>
 )
