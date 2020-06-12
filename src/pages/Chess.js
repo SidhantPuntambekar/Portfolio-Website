5e2ff62050a460c2bef7c325621e27f310468df4
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-12 14:54:27 -0600

    Added chess webpage

---
+++ src/pages/Chess.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+import "../styles/Chess.css"
+
+export default () =>
+(
+    <Layout>
+        <h2>Chess</h2>
+        <p>
+        Welcome to the chess page of my website. This page is meant to highlight my favorite chess games throuhgout the years.
+        </p>
+        <h2>1972 World Chess Championship</h2>
+    </Layout>
+)
