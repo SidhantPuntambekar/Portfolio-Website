982cb370d7e7f5f444c99a5f5a6caef1e2785cf5
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-19 11:21:53 -0600

    Fixed fractions in background and added picture of Tigran Petrosian

--- src/pages/Chess.js
+++ src/pages/Chess.js
@@ -16,7 +16,7 @@ export default () =>
             <br></br>-Jim Slater</b></i>
             <br></br>
             <br></br>
-            The 1972 World Chess Championship was played between World Chess Champion Boris Vasiljevic Spassky of the Soviet Union and Challenger Robert James Fischer of the United States. It was held in Reykjavik, Iceland during the height of the Cold War. Previously, Spassky had won the Candidates tournament of 1966 and 1969 and faced World Champion Tigran Petrosian losing to him in 1966 by a margin of 12<sup>1</sup>&frasl;<sup>2</sup> to 11<sup>1</sup>&frasl;<sup>2</sup> and beating him in 1969 by a margin of 12&frac12; to 10&frac12;. Spassky was known as a player that 
+            The 1972 World Chess Championship was played between World Chess Champion Boris Vasiljevic Spassky of the Soviet Union and Challenger Robert James Fischer of the United States. It was held in Reykjavik, Iceland during the height of the Cold War. Previously, Spassky had won the Candidates tournament of 1966 and 1969 and faced World Champion Tigran Petrosian losing to him in 1966 by a margin of 12&frac12; to 11&frac12; and beating him in 1969 by a margin of 12&frac12; to 10&frac12;. Spassky was known as a player that 
             <figure>
                 <img height = "200" src = "https://d53j7mnhlzrdg.cloudfront.net/series/best-games-of-boris-spassky.jpg"></img>
                 <figcaption>Boris Vasiljevic Spassky, World Chess Defending Champion, USSR</figcaption>
@@ -26,8 +26,8 @@ export default () =>
                 <figcaption>Robert James Fischer, World Chess Championship Challenger, USA</figcaption>
             </figure>
             <figure>
-                <img height = "200" src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thetimes.co.uk%2Farticle%2Fjim-slater-b5jnf2jxc&psig=AOvVaw2uygUA2I60qD3VmMUnEw-t&ust=1592672660334000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMC66JmujuoCFQAAAAAdAAAAABAD"></img>
-                <figcaption>Jim Slater</figcaption>
+                <img height = "200" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Tigran_Petrosian_1962.jpg/220px-Tigran_Petrosian_1962.jpg"></img>
+                <figcaption>Tigran Petrosian</figcaption>
             </figure>
             <figure>
                 <img height = "200" src = "https://www.chesshistory.com/winter/pics/cn5198_fischer.jpg"></img>
