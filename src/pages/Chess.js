a5a6a7e13528b6ccb3e19a6cf80877cb3d751e7c
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-06-19 12:45:47 -0600

    Fixed formatting

--- src/pages/Chess.js
+++ src/pages/Chess.js
@@ -28,8 +28,6 @@ export default () =>
             One day before the Match of the Century was supposed to take place, Fischer refused to fly from New York to Reykjavik for monetary reasons and thus the match was postponed by an additional two days. Jim Slater, who was a British investor, offered to double the prize fund of the tournament to $250,000. Former US Secretary of State, Henry Kissinger also pleaded with Fischer to play Spassky in Reykjavik. Eventually, Fischer flew to Reykjavik but the match was still postponed because Spassky demanded that Fischer apologize to FIDE for hijacking the terms of the match. Fischer apologized publicly to Spassky and the match started on July 11th, 1972.    
             <br></br>
             <br></br>
-            <h3>Game 1: Nimzo-Indian Defense, Normal Variation. Gligoric System, Bernstein Defense (E56)</h3>
-            <b>Move Order: 1.d4 Nf6 2.c4 e6 3.Nf3 d5 4.Nc3 Bb4 5.e3 0-0 6.Bd3 c5 7.0-0 Nc6 8.a3 Ba5 9.Ne2 dxc4 10.Bxc4 Bb6 11.dxc5 Qxd1 12.Rxd1 Bxc5 13.b4 Be7 14.Bb2 Bd7 15.Rac1 Rfd8 16.Ned4 Nxd4 17.Nxd4 Ba4 18.Bb3 Bxb3 19.Nxb3 Rxd1+ 20.Rxd1 Rc8 21.Kf1 Kf8 22.Ke2 Ne4 23.Rc1 Rxc1 24.Bxc1 f6 25.Na5 Nd6 26.Kd3 Bd8 27.Nc4 Bc7 28.Nxd6 Bxd6 29.b5 Bxh2 30.g3 h5 31.Ke2 h4 32.Kf3 Ke7 33.Kg2 hxg3 34.fxg3 Bxg3 35.Kxg3 Kd6 36.a4 Kd5 37.Ba3 Ke4 38.Bc5 a6 39.b6 f5 40.Kh4 f4 41.exf4 Kxf4 42.Kh5 Kf5 43.Be3 Ke4 44.Bf2 Kf5 45.Bh4 e5 46.Bg5 e4 47.Be3 Kf6 48.Kg4 Ke5 49.Kg5 Kd5 50.Kf5 a5 51.Bf2 g5 52.Kxg5 Kc4 53.Kf5 Kb4 54.Kxe4 Kxa4 55.Kd5 Kb5 56.Kd6</b>
             <figure>
                 <img height = "200" src = "https://d53j7mnhlzrdg.cloudfront.net/series/best-games-of-boris-spassky.jpg"></img>
                 <figcaption>Boris Vasiljevic Spassky, World Chess Defending Champion, USSR</figcaption>
@@ -46,6 +44,9 @@ export default () =>
                 <img height = "200" src = "https://www.chesshistory.com/winter/pics/cn5198_fischer.jpg"></img>
                 <figcaption>Newspaper headline indicating the increase of prize fund money.</figcaption>
             </figure>
+            <br></br>
+            <h3>Game 1: Nimzo-Indian Defense, Normal Variation. Gligoric System, Bernstein Defense (E56)</h3>
+            <b>Move Order: 1.d4 Nf6 2.c4 e6 3.Nf3 d5 4.Nc3 Bb4 5.e3 0-0 6.Bd3 c5 7.0-0 Nc6 8.a3 Ba5 9.Ne2 dxc4 10.Bxc4 Bb6 11.dxc5 Qxd1 12.Rxd1 Bxc5 13.b4 Be7 14.Bb2 Bd7 15.Rac1 Rfd8 16.Ned4 Nxd4 17.Nxd4 Ba4 18.Bb3 Bxb3 19.Nxb3 Rxd1+ 20.Rxd1 Rc8 21.Kf1 Kf8 22.Ke2 Ne4 23.Rc1 Rxc1 24.Bxc1 f6 25.Na5 Nd6 26.Kd3 Bd8 27.Nc4 Bc7 28.Nxd6 Bxd6 29.b5 Bxh2 30.g3 h5 31.Ke2 h4 32.Kf3 Ke7 33.Kg2 hxg3 34.fxg3 Bxg3 35.Kxg3 Kd6 36.a4 Kd5 37.Ba3 Ke4 38.Bc5 a6 39.b6 f5 40.Kh4 f4 41.exf4 Kxf4 42.Kh5 Kf5 43.Be3 Ke4 44.Bf2 Kf5 45.Bh4 e5 46.Bg5 e4 47.Be3 Kf6 48.Kg4 Ke5 49.Kg5 Kd5 50.Kf5 a5 51.Bf2 g5 52.Kxg5 Kc4 53.Kf5 Kb4 54.Kxe4 Kxa4 55.Kd5 Kb5 56.Kd6</b>
         </p>
     </Layout>
 )
