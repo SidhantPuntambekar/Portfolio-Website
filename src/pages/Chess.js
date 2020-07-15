cdfc4666aed6d9815990297666a108c0b08ab47e
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-15 11:27:13 -0600

    Fixed typo

--- src/pages/Chess.js
+++ src/pages/Chess.js
@@ -7,7 +7,7 @@ export default () =>
     <Layout>
         <h2>Chess</h2>
         <p>
-        Welcome to the chess page of my website. This page is meant to highlight my favorite chess games throuhgout the years.
+        Welcome to the chess page of my website. This page is meant to highlight my favorite chess games throughout the years.
         </p>
         <h2>1972 World Chess Championship</h2>
         <p>
