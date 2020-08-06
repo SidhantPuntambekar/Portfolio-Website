8d10ab3895a37071645a225b9639aa64e3c04eb6
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-08-06 11:15:26 -0600

    Changed resume page text

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,14 +4,13 @@ import Layout from "../components/Layout"
 export default () => (
   <Layout>
     <p>
-      This page is a work in progress! In the meantime, feel free to check out
-      my{" "}
+      Welcome to my Resume page. Check out my{" "}
       <a
         href={
           "https://drive.google.com/file/d/1fK9AA9fNa7nWl52VrlSl9GBVqzF0jzKe/view?usp=sharing"
         }
       >
-        résumé
+        Resume
       </a>{" "}
       or my <a href={"https://www.github.com/SidhantPuntambekar"}>GitHub</a>.
     </p>
