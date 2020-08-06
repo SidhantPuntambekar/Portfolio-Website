920c2973c0e46b9c3e90d827a586bad3fce55de5
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-08-06 09:30:30 -0600

    Changed text

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,8 +4,8 @@ import Layout from "../components/Layout"
 export default () => (
   <Layout>
     <p>
-      Check out my resume here:{" "}
-      <a href={"../ResumePuntambekarWebsite.pdf"}>Resume</a> or my GitHub at:{" "}
+      This page is a work in progress! In the meantime, feel free to check out
+      my <a href={"../ResumePuntambekarWebsite.pdf"}>résumé</a> or my{" "}
       <a href={"https://www.github.com/SidhantPuntambekar"}>GitHub</a>.
     </p>
   </Layout>
