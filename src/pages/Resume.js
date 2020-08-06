b1153447f6b30daf30315cb8aa6ae5b75655220e
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-08-06 10:33:56 -0600

    Updated resume link

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,8 +5,15 @@ export default () => (
   <Layout>
     <p>
       This page is a work in progress! In the meantime, feel free to check out
-      my <a href={"../ResumePuntambekarWebsite.pdf"}>résumé</a> or my{" "}
-      <a href={"https://www.github.com/SidhantPuntambekar"}>GitHub</a>.
+      my{" "}
+      <a
+        href={
+          "https://drive.google.com/file/d/1fK9AA9fNa7nWl52VrlSl9GBVqzF0jzKe/view?usp=sharing"
+        }
+      >
+        résumé
+      </a>{" "}
+      or my <a href={"https://www.github.com/SidhantPuntambekar"}>GitHub</a>.
     </p>
   </Layout>
 )
