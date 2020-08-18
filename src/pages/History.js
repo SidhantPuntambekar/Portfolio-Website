fbb47af1b45b46f638b776be827f699ba70558db
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-08-18 11:15:50 -0600

    Added Battle of Tulagi

--- src/pages/History.js
+++ src/pages/History.js
@@ -353,7 +353,6 @@ export default () => (
         </figcaption>
       </figure>
     </p>
-
     <h2>The Beginning</h2>
     <p>
       <h3>Pearl Harbor: December 7th, 1941</h3>
@@ -938,9 +937,7 @@ export default () => (
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/AkagiDeckApril42.jpg/220px-AkagiDeckApril42.jpg"
           alt="Akagi"
         ></img>
-        <figcaption>
-          Japanese carrier Akagi served as Japanese flagship.
-        </figcaption>
+        <figcaption>Japanese carrier Akagi served as flagship.</figcaption>
       </figure>
       <figure>
         <img
@@ -949,7 +946,7 @@ export default () => (
           alt="TBD1 Devestator"
         ></img>
         <figcaption>
-          TBD Devestator torpedo bombers on the flight deck on USS Enterprise.
+          TBD Devastator torpedo bombers on the flight deck on USS Enterprise.
         </figcaption>
       </figure>
       <figure>
@@ -1001,10 +998,84 @@ export default () => (
         </figcaption>
       </figure>
     </p>
-
     <h2>The Solomon Islands Campaign</h2>
-    <p>
-      <h3>Battle of Tulagi and Gavutu- Tanambogo</h3>
-    </p>
+    <h3>Battle of Tulagi and Gavutu-Tanambogo: August 7-9 1942</h3>
+    With the momentum following the sinking of four Japanese aircraft carriers
+    at Midway, the United States was eager to open up an offensive in the
+    Japanese controlled South Pacific. The Japanese had plans to invade New
+    Guinea and continue with the invasion of Australia and New Zealand. A
+    Japanese occupation of Australia and New Zealand would cut the British and
+    American trade routes and ties in the South Pacific. The United States
+    launched an invasion of the Solomon Islands where the Japanese had several
+    strategic airfields and naval bases (Rabaul being the primary one). The
+    United States chose to invade the small islands of Tulagi and
+    Gavutu-Tanambogo to establish a beachead near Florida and Guadalcanal
+    islands. From Tulagi and Gavutu-Tanambogo, the Americans could turn their
+    attention to the much larger islands in the Solomon chain and retake them
+    one by one. Douglas SBD Dauntless dive bombers from the new aircraft carrier
+    USS Wasp destroyed Japanese shore facilities and anti-aircraft nests while
+    Grumman F4F Wildcats strafed Japanese fighters and sea planes that were
+    anchored in the harbors. The Americans also had naval bombardment with the
+    cruiser USS San Juan and destroyers USS Monseen and USS Buchanan. US Marines
+    soon landed on Tulagi at 8:00 AM on August 7th and established a beachead
+    unopposed and began marching to the strategic outpost of Hill 281. By
+    nightfall, the Marines had captured almost 2/5ths of Tulagi without
+    encountering too much Japanese resistance. However, the Japanese
+    counterattacked at night and made slight progress by pushing the Marines
+    back. The resistance on Tulagi could not destroy the Americans completely
+    and by the afternoon of August 8th, Tulagi was confidently in American
+    hands.
+    <br></br>
+    <br></br>
+    The Marines also landed on Gavutu and Tanambogo islands at noon on August
+    7th. The occupation followed a similar cadence to the occupation of Tulagi
+    where US Marines made major progress but had to stop at nightfall. The
+    Japanese attacked in the night but failed to destroy the majoritry of the
+    American force. Constant naval bombardment also allowed the islets to be
+    captured by the end of August 9th. The United States lost 122 men occupying
+    the islands while the Japanese lost 863 men. After establishing control of
+    Tulagi and Gavutu-Tanambogo, General Alexander Vandgrift landed 11,000
+    Marines on the island of Guadalcanal. The Americans captured the strategic
+    Japanese airfield at the Lunga River and renamed it Henderson Field. This
+    airfield would become extremely important for control of Guadalcanal and the
+    rest of the Solomon islands campaign.
+    <br></br>
+    <figure>
+      <img
+        height="200"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/TulagiBeachBlue.jpg/300px-TulagiBeachBlue.jpg"
+        alt="Landing at Tulagi"
+      ></img>
+      <figcaption>US Marines land ashore on Tulagi</figcaption>
+    </figure>
+    <figure>
+      <img
+        height="200"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/TulagiBattle1942.gif/220px-TulagiBattle1942.gif"
+        alt="Aerial of Tulagi"
+      ></img>
+      <figcaption>
+        Aerial photo of the US Marine advance to Hill 281 on Tulagi
+      </figcaption>
+    </figure>
+    <figure>
+      <img
+        height="200"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/TulagiRupertus.jpg/220px-TulagiRupertus.jpg"
+        alt="Rupertus superivising Tulagi assault"
+      ></img>
+      <figcaption>General Rupertus observes the landings on Tulagi</figcaption>
+    </figure>
+    <figure>
+      <img
+        height="200"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/TulagiMarineOfficers.jpg/220px-TulagiMarineOfficers.jpg"
+        alt="US Marines on Tulagi"
+      ></img>
+      <figcaption>
+        US Marine officers after the successful capture of Tulagi
+      </figcaption>
+    </figure>
+    <h3>Battle of Savo Island: August 8-9 1942</h3>
   </Layout>
 )
