adbf99c4300896ca5f1d711f3c2da710d4e6eb51
Sidhant Puntambekar <sidhantnp@yahoo.com>
2021-06-07 11:44:32 -0600

    Latest commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: { path: `${__dirname}/assets/` },
-      icon: `static/favicon.ico`,
+      icon: `static/images/gatsby-config.png`,
     },
   ],
 }
