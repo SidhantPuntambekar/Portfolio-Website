7ccffcb0d73bc9eb8160ff07b5b26559601462a3
Sidhant Puntambekar <sidhantnp@yahoo.com>
2021-07-22 21:55:29 -0600

    Tried to fix html.js

--- src/html.js
+++ src/html.js
@@ -1,33 +1,38 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from "react"
+import PropTypes from "prop-types"
 
-export default function HTML(props) 
-{
-    return (
-		<html {...props.htmlAttributes} lang={"en"}>
-			<head>
-				<meta charSet="utf-8" />
-				<meta httpEquiv="x-ua-compatible" content="ie=edge" />
-				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-				{props.headComponents}
-				<title>Sidhant Puntambekar</title>
-				<link rel={"icon"} href={"images/me1.jpg"} />
-			</head>
-			<body {...props.bodyAttributes}>
-				{props.preBodyComponents}
-				<div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
-				{props.postBodyComponents}
-			</body>
-		</html>
-	)
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes} lang={"en"}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+        <title>Sidhant Puntambekar</title>
+        <link rel={"icon"} href={"../images/me.jpg"} />
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
 }
 
-HTML.propTypes = 
-{
-	htmlAttributes: PropTypes.object,
-	headComponents: PropTypes.array,
-	bodyAttributes: PropTypes.object,
-	preBodyComponents: PropTypes.array,
-	body: PropTypes.string,
-	postBodyComponents: PropTypes.array,
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
 }
