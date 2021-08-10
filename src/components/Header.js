e850b4fb042f867e84a0b0117776c8af76767db5
Sidhant Puntambekar <sidhantnp@yahoo.com>
2021-08-10 17:01:23 -0600

    Added meta html tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Navbar from "./Navbar";
-import "../styles/Header.css";
+import React from "react"
+import Navbar from "./Navbar"
+import "../styles/Header.css"
 
 export default () => (
-	<header>
-		<div id="title-container">
-			<h1 id="title">Sidhant Puntambekar</h1>
-			<span id="secret-tooltip">Read about me on the home page.</span>
-		</div>
-		<Navbar />
-	</header>
+  <header>
+    <div id="title-container">
+      <h1 id="title">Sidhant Puntambekar</h1>
+      <span id="secret-tooltip">Read about me on the home page.</span>
+    </div>
+    <Navbar />
+  </header>
 )
--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React from "react"
 
 export default () => (
-	<>
-		<title>Sidhant Puntambekar</title>
-		<link rel="icon" href="" />
-	</>
+  <>
+    <meta
+      name="google-site-verification"
+      content="OIwwJxbIklRLY5Iyho4fkbQcM6OHaF5iaC7d2w1mFGE"
+    />
+    <title>Sidhant Puntambekar</title>
+    <link rel="icon" href="" />
+  </>
 )
